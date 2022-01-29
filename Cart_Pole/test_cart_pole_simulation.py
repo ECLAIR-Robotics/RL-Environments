834f47e7e4f3ceb89bbc4ddf4fe44a4fef965f47
Conrad <conradliste@gmail.com>
2022-01-29 04:38:11 -0600

    Reorganized things and added robot arm pybullet

--- Cart_Pole/test_cart_pole_simulation.py
+++ Cart_Pole/test_cart_pole_simulation.py
@@ -1,11 +1,11 @@
-from pybullet_envs.bullet.cartpole_bullet import CartPoleContinuousBulletEnv
+from pybullet_envs.bullet.cartpole_bullet import CartPoleBulletEnv
 import gym
 
 max_step = 1000
 episode_count = 0
 max_episodes = 50
 
-env = CartPoleContinuousBulletEnv(renders=True)
+env = CartPoleBulletEnv(renders=True)
 observation = env.reset()
 while episode_count < max_episodes:
   env.render()
---
+++ test_robot_arm.py
@@ -0,0 +1,20 @@
+import pybullet_envs.bullet.minitaur_gym_env as e
+
+
+
+max_step = 1000
+episode_count = 0
+max_episodes = 50
+
+env = e.MinitaurBulletEnv(render=True)
+
+observation = env.reset()
+while episode_count < max_episodes:
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  observation, reward, done, info = env.step(action)
+  # Rest environment if done
+  if done:
+    observation = env.reset()
+    episode_count += 1
+env.close()
