83f4cf94c79d21a3b01f63777c429290e35b57ec
Conrad <conradliste@gmail.com>
2022-04-02 12:32:08 -0500

    Finished ppo

--- ppo.py
+++ ppo.py
@@ -2,14 +2,17 @@ from actor import Actor
 from critic import Critic
 from replay_buffer import ReplayBuffer
 from pybullet_envs.bullet.kukaGymEnv import KukaGymEnv
+import torch
+import torch.functional as F
 import torch.optim as optim
+from torch.distributions import MultivariateNormal  # TODO: Change this to a custom more stable normal distribution
 
 class PPO:
     def __init__(self, hyperparameters):
         """
         Args:
             hyperparameters (dict): a dictionary of hyperparameters
-        Returns:
+        discounted_returns:
             None
         """
         # Extract hyperparameters
@@ -19,7 +22,9 @@ class PPO:
         self.state_dim = hyperparameters['state_dim']
         self.action_dim = hyperparameters['action_dim']
         self.total_train_steps = hyperparameters['total_train_steps']
-        self.num_rand_epochs = hyperparameters['num_rand_epochs']
+        self.max_episode_length = hyperparameters['max_episode_length']
+        self.num_train_epochs = hyperparameters['num_train_epochs']
+        self.device = hyperparameters['device']
         # Initialize actor/critic networks
         self.actor = Actor(self.state_dim, self.action_dim)
         self.critic = Critic(self.state_dim, self.action_dim)
@@ -29,51 +34,98 @@ class PPO:
         self.replay_buffer = ReplayBuffer(self.batch_size)
         self.enviroment = KukaGymEnv(renders=True)
 
-    def sample_action_space(self):
+    def sample_policy(self, total_time_steps=None):
         """
-        Uniformly samples an action from the action space
+        Uses the policy to interact with the environment and fills the replay buffer
+        
+        Args:
+            total_time_steps (int): the minimum total number of time steps to sample
         """
-        return self.enviroment.action_space.sample()
-
-    def sample_policy(self, total_time_steps):
-        """
-        Uses the policy to interact with the environment and fill replay buffer
-        """
-        # Sample certain number of transitions
+        if total_time_steps is None:
+            total_time_steps = self.num_batch_transitions
+        # Sample until at least have total_time_steps samples
         num_samples = 0
         while num_samples < total_time_steps:
             done = False
             cur_state = self.enviroment.reset()
-            # Run episodes until we get enough samples
-            for i in range(10000000):
-                # Sample action from the policy
+            # Run full episodes until we get enough samples
+            for i in range(self.max_episode_length):
+                # Sample and perform action from the policy
                 action, log_prob = self.actor.sample_action()
-                # Take action in the environment
                 next_state, reward, done, info = self.enviroment.step(action)
                 # Store transition in replay buffer
                 num_samples += 1
-                self.replay_buffer.append(cur_state, action, reward, next_state, done, (log_prob))
+                self.replay_buffer.append(cur_state, action, reward, next_state, done, log_prob)
                 cur_state = next_state
-                # End the environment if done 
                 if done:
                     break
+        
+    def rewards_to_go(self, sampled_rewards, sampled_dones, normalize=False):
+        """
+        Computes discounted returns for each episode from the rewards with the option of normalizing them 
 
+        Args:
+            sampled_rewards (Iterable[Float]): an iterable containing the rewards
+            sampled_dones (Iterable[Bool]): an iterable containing whether there was terminal time step
+            normalize (Bool): whether to normalize the returns
+        """
+
+        discounted_returns = []
+        disc_return = 0
+        # Compute discounted return for each episode
+        for reward, done in zip(reversed(sampled_rewards), reversed(sampled_dones)):
+            if done:
+                disc_return = 0
+            disc_return = reward + self.discount * disc_return
+            discounted_returns.insert(0, disc_return) # TODO: Try to append and then reverse
+        
+        discounted_returns = torch.tensor(discounted_returns, dtype=torch.float32).to(self.device)
+        # Normalize discounted_returns
+        if normalize:
+            discounted_returns = (discounted_returns - discounted_returns.mean()) / (discounted_returns.std() + 1e-8)
+        return discounted_returns
+    
     def train(self):
         """
         Trains the actor and critic networks using PPO
         """
         print("Training PPO for {} steps".format(self.total_train_steps))
         num_time_steps = 0
-        while num_time_steps < self.total_train_steps:
-            # Sample policy to fill replay buffer
-            self.sample_policy()
-            state, action, reward, next_state, done, info = self.sample_transitions(return_type="torch_tensor")
-            num_time_steps += 1
-            # Compute and normalize advantage
-            value = self.critic(state)
-            next_value = self.critic(next_state)
-            
+        num_train_iterations = 0
 
+        while num_time_steps < self.total_train_steps:
+            # Sample policy for transitions
+            self.sample_policy()
+            state, action, reward, next_state, done, action_log_prob = self.sample_transitions(return_type="torch_tensor")
+            # Compute advantage and value
+            returns = self.rewards_to_go(reward, done)
+            value = self.critic(state, action)
+            advantage = returns - value.detach()
+            # Normalize advantage
+            advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-10)
+            # Update actor and critic networks
+            for train_epoch in range(self.num_train_epochs):
+                # Compute value and action prob ratio
+                value = self.critic(state, action) # TODO: See if you can just resuse value outside for loop
+                action, cur_action_log_prob = self.actor.sample_action() 
+                action_prob_ratio = torch.exp(cur_action_log_prob - action_log_prob)
+                # Compute actor loss
+                loss1 = action_prob_ratio * advantage
+                loss2 = torch.clamp(action_prob_ratio, 1-self.clip, 1+self.clip ) * advantage
+                actor_loss = -torch.min(loss1, loss2).mean()
+                # Compute critic loss
+                critic_loss = F.mse_loss(returns, value) 
+                # Update actor network
+                self.actor_optim.zero_grad()
+                actor_loss.backward(retain_graph=True) # retain_graph prevents gradients from being erased after backprop
+                self.actor_optim.step()
+                # Update critic network
+                self.critic_optim.zero_grad()
+                critic_loss.backward()
+                self.critic_optim.step()
+            
+            self.buffer.clear()
+            num_time_steps += len(state)
 
     def test(self):
         return
--- replay_buffer.py
+++ replay_buffer.py
@@ -22,23 +22,40 @@ class ReplayBuffer():
 
     def sample_indices(self, num_samples, replace):
         """
-        Samples a batch of indices from the replay buffer
+        Samples a random batch of indices from the replay buffer
         """
-        return np.random.choice(len(self.memory), num_samples, replace=replace) 
+        return np.random.choice(len(self.memory), num_samples, replace=replace)
 
-    def get_transitions(self, sampled_indices, return_type):
+    def get_transitions(self, return_type, indices=None, device=None):
         """
-        Gets the transitions at the given indices
+        Gets all the transitions or just those at given indices
+
+        Args:
+            return_type (str): the type of the return value
+            indices (Iterable[int]): the indices of the transitions to return
+            device (torch.device): the device to return the transitions to
+        
+        Returns:
+            state: len(indices) x state_dim iterable of states
+            action: len(indices) x action_dim iterable of actions
+            reward: len(indices) iterable of rewards
+            next_state: len(indices) x state_dim iterable of next states
+            done: len(indices) iterable of booleans indicating whether the episode has ended
+            info: len(indices) x info_dim iterable of extra information
         """
-        # Extract the transitionse
-        transitions = [self.memory[i] for i in sampled_indices]
+        # Extract the transitions from indices
+        if indices is None:
+            transitions = list(self.memory)
+        else:
+            transitions = [self.memory[i] for i in indices]
+
         state, action, reward, next_state, done, info = zip(*transitions)
         
         # Data conversions
         if return_type == 'numpy':
             return np.array(state), np.array(action), np.array(reward), np.array(next_state), np.array(done), info
-        elif return_type == "torch_tensor":
-            return torch.tensor(state), torch.tensor(action), torch.tensor(reward), torch.tensor(next_state), torch.tensor(done), info
+        elif return_type == "torch":
+            return torch.tensor(state, dtype=torch.float32), torch.tensor(action, dtype=torch.float32), torch.tensor(reward, dtype=torch.float32), torch.tensor(next_state, dtype=torch.float32), torch.tensor(done), info
         elif return_type == 'tuple':
             return state, action, reward, next_state, done, info
     
@@ -47,8 +64,8 @@ class ReplayBuffer():
         Samples a batch of transitions from the replay buffer
         """
         sampled_indices = self.sample_indices(num_samples, replace)
-        return self.get_transitions(sampled_indices, return_type)
-    
+        return self.get_transitions(return_type, sampled_indices=sampled_indices)
+
     def clear(self):
         """
         Clears all the data from the buffer
@@ -66,6 +83,7 @@ class ReplayBuffer():
             reward: the reward received after taking the action
             next_state: the next state of the environment after taking the action
             done: whether the episode has ended after taking the action
+            info: extra information about the transition (e.g. log probs)
         """
         self.memory.append((state, action, reward, next_state, done, info))
         return
@@ -76,7 +94,16 @@ class ReplayBuffer():
 if __name__ == "__main__":
     buffer = ReplayBuffer(10)
     for i in range(10):
-        buffer.append(1, 2, 3, 4, 5)
-    state, action, reward, next_state, done = buffer.sample(3)
+        buffer.append(1, 2, 3, 4, True)
+    state, action, reward, next_state, done, info = buffer.sample_transitions(3)
     print(state)
     print(action)
+    print(reward)
+    print(done)
+    print(info)
+    state, action, reward, next_state, done, info = buffer.get_transitions('torch')
+    print(state)
+    print(action)
+    print(reward)
+    print(done)
+    print(info)
